3064af5ace94613989bbe65fe25d7ad73b80fd73
ggyy456 <30652294+ggyy456@users.noreply.github.com>
2017-08-27 17:27:58 +0800

    atomikos事务更改

--- src/main/java/com/mjx/interceptor/MyInterceptor.java
+++ src/main/java/com/mjx/interceptor/MyInterceptor.java
@@ -24,15 +24,15 @@ public class MyInterceptor {
         if (!flag){
             FLAG_HOLDER.set(true);
             LOGGER.error("方法 : " + pJoinPoint.getTarget().getClass() + "." + pJoinPoint.getSignature().getName());
-            try {
+            //try {
                 result = pJoinPoint.proceed();
-            } catch (Exception e) {
-                e.printStackTrace();
-                LOGGER.error("出现异常");
-            } finally {
+            //} catch (Exception e) {
+            //    e.printStackTrace();
+            //    LOGGER.error("出现异常");
+            //} finally {
                 LOGGER.error("退出");
                 FLAG_HOLDER.remove();
-            }
+            //}
         }
 
         return result;
