4ab67c9a08d8106e2439f661bc20e9594954defa
ggyy456 <30652294+ggyy456@users.noreply.github.com>
2017-09-02 10:37:40 +0800

    脚本更新

--- src/test/java/bat/直接执行.bat
+++ src/test/java/bat/直接执行.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" /d "E:\java_idea\Apache24\bin\" "ab.exe" -n 5000 -c 500 -k http://localhost:8081/frame/login.action
+start "" /d "E:\java_idea\Apache24\bin\" "ab.exe" -n 5000 -c 500 -k http://localhost:8081/ticket/login.do
