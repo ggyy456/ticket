b2012420087cc8807dae9c87759c9d4314effd27
ggyy456 <30652294+ggyy456@users.noreply.github.com>
2017-09-18 21:47:59 +0800

    数据初始化

--- src/test/java/com/mjx/test/TestTicket.java
+++ src/test/java/com/mjx/test/TestTicket.java
@@ -5,21 +5,14 @@ import com.mjx.entity.Train;
 import com.mjx.util.UUIDGenerator;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017-9-18.
  */
 public class TestTicket {
     public static void main(String[] args) {
-
-
-
-
-        System.out.println(UUIDGenerator.getUUID());
+        listToDatabase(databaseToList());
     }
 
     public static List<Train> databaseToList(){
@@ -37,7 +30,7 @@ public class TestTicket {
             System.out.println("数据库连接成功！！！");
 
             String sql="select TRAIN_ID,TRAIN_TYPE From T_TRAIN "
-                    +"where BEGIN_STATION='北京' and END_STATION in('上海','天津')";
+                    +"where BEGIN_STATION='北京' and END_STATION in('上海','天津','重庆')";
 
             stmt = (Statement) conn.createStatement();
             stmt.execute(sql);//执行select语句用executeQuery()方法，执行insert、update、delete语句用executeUpdate()方法。
@@ -55,6 +48,8 @@ public class TestTicket {
             conn.close();
 
             System.out.println("导入完成！！！");
+
+            return list;
 
         }catch(Exception e){
             System.out.println(e.getMessage());
@@ -77,16 +72,41 @@ public class TestTicket {
             System.out.println("数据库连接成功！！！");
 
             String sql="insert into t_ticket(train_id,ticket_no,ticket_time,ticket_type,is_sell) values(?,?,?,?,?)";
-
             pstmt = conn.prepareStatement(sql);
+            Map<String,String[]> map = new HashMap<String,String[]>();
+            map.put("G",ConstantTicket.TICKET_TYPE_G);
+            map.put("C",ConstantTicket.TICKET_TYPE_C);
+            map.put("D",ConstantTicket.TICKET_TYPE_D);
+            map.put("Z",ConstantTicket.TICKET_TYPE_Z);
+            map.put("T",ConstantTicket.TICKET_TYPE_T);
+            map.put("K",ConstantTicket.TICKET_TYPE_K);
 
-            String[] hotCity = ConstantTicket.HOT_CITY;
-            Random rd = new Random();
+            Map<String,int[]> numMap = new HashMap<String,int[]>();
+            numMap.put("G",ConstantTicket.TICKET_TYPE_G_NUM);
+            numMap.put("C",ConstantTicket.TICKET_TYPE_C_NUM);
+            numMap.put("D",ConstantTicket.TICKET_TYPE_D_NUM);
+            numMap.put("Z",ConstantTicket.TICKET_TYPE_Z_NUM);
+            numMap.put("T",ConstantTicket.TICKET_TYPE_T_NUM);
+            numMap.put("K",ConstantTicket.TICKET_TYPE_K_NUM);
 
             for (int i = 0; i < list.size(); i++) {
+                int trainId = list.get(i).getTrainId();
+                String trainType = list.get(i).getTrainType();
+                String[] ticketTypes = map.get(trainType);
+                int[] ticketTypeNums = numMap.get(trainType);
 
+                for (int j = 0; j < ticketTypes.length; j++) {
+                    for (int k = 0; k < ticketTypeNums[j]; k++) {
+                        System.out.println(trainId);
+                        pstmt.setInt(1,trainId);
+                        pstmt.setString(2,UUIDGenerator.getUUID());
+                        pstmt.setString(3,"2017-09-18");
+                        pstmt.setString(4,ticketTypes[j]);
+                        pstmt.setString(5,"0");
+                        pstmt.execute();
+                    }
+                }
             }
-
 
             pstmt.close();
             conn.close();
