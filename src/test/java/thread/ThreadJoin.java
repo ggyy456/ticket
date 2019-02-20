4dee247dac86b9104abf14028da19c048d596e65
ggyy456 <42746545@qq.com>
2019-02-20 11:11:11 +0800

    线程例子

---
+++ src/test/java/thread/ThreadJoin.java
@@ -0,0 +1,56 @@
+package thread;
+
+/*
+    join的作用，等待子线程运行完毕后，才能执行主线程的代码
+    Thread.sleep方法不释放锁，而join是会释放锁的
+ */
+public class ThreadJoin extends Thread {
+    @Override
+    public void run(){
+        try{
+            System.out.println(Thread.currentThread().getName()+"：begin");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName()+"：end");
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    synchronized  public void last(){
+        System.out.println(Thread.currentThread().getName()+"：last");
+    }
+
+    public static void main(String[] args) {
+        final ThreadJoin tj = new ThreadJoin();
+        tj.setName("tj");
+
+        Thread t = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized (tj) {
+                        System.out.println(Thread.currentThread().getName()+"：begin");
+                        tj.start();
+                        //tj.join();
+                        Thread.sleep(3000);
+                        System.out.println(Thread.currentThread().getName()+"：end");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.setName("t");
+        t.start();
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        tj.last();
+        //System.out.println(Thread.currentThread().getName()+"：last");
+    }
+
+}
